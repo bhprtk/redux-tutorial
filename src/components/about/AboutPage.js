12337d5660e08ebf3928bb449306d2eabd747a2b
pratik <lazy.monk.13@gmail.com>
2016-08-09 19:09:18 -0700

    add all routes and components for them

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 class AboutPage extends React.Component {
 	render() {
-		<div>
-			<h1>About</h1>
-			<p>This is the about page</p>
-		</div>
+		return (
+			<div>
+				<h1>About</h1>
+				<p>This is the about page</p>
+			</div>
+
+		);
 	}
 }
 
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router';
 
 class HomePage extends React.Component {
-	rendeR() {
+	render() {
 		return (
 			<div className="jumbotron">
 				<h1>Home Page</h1>
