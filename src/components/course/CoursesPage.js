ca1e7a472f052536fc444dd9d6f5b0d599a646d3
pratik <lazy.monk.13@gmail.com>
2016-08-12 22:05:32 -0700

    add actionTypes file to store constants

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,6 +1,8 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+
 
 class CoursesPage extends React.Component {
 
@@ -22,7 +24,7 @@ class CoursesPage extends React.Component {
 	}
 
 	onClickSave() {
-		this.props.createCourse(this.state.course);
+		this.props.actions.createCourse(this.state.course);
 	}
 
 	courseRow(course, index) {
@@ -52,7 +54,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
 	courses: PropTypes.array.isRequired,
-	createCourse: PropTypes.func.isRequired
+	actions: PropTypes.object.isRequired
 
 };
 
@@ -64,7 +66,7 @@ function mapStateToProps(state, ownProps) {
 
 function mapDispatchToProps(dispatch) {
 	return {
-		createCourse: course => dispatch(courseActions.createCourse(course))
+		actions: bindActionCreators(courseActions, dispatch)
 	};
 }
 
