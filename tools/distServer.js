44e0a37a8249fd02fcd20c50b1b1f1c1f568ea64
pratik <lazy.monk.13@gmail.com>
2016-08-22 20:38:19 -0700

    serving in production mode

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-
+import compression from 'compression';
 /*eslint-disable no-console */
 
 const PORT = 3000;
 const app = express();
 
+app.use(compression());
 app.use(express.static('dist'));
 
 app.get('*', function(req, res) {
